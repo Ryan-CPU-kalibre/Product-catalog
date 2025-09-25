12c9d308c132fb642501dac9041370b1ec2c0b74
Ryan-CPU-kalibre <60849591+Ryan-CPU-kalibre@users.noreply.github.com>
2025-09-25 17:51:59 -0600

    s commit

--- AleStore/settings.py
+++ AleStore/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost, product-catalog-s63w.onrender.com", ).split(",")
 
 # Application definition
 INSTALLED_APPS = [
